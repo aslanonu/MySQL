bb123961a0f18f1bdc553e614620dad4e687f7ca
aslanonu <81285472+aslanonu@users.noreply.github.com>
2021-06-17 07:45:42 -0700

    Membuat PerintahMySQL.php

---
+++ PerintahMySQL.php
@@ -0,0 +1,9 @@
+Perintah- perintah MySQL untuk mengelola database
+1. create database nama_database;
+2. perintah drop database nama_database;
+3. Perintah untuk membuat tabel digunakan perintah create table nama_table ( nama tabel (panjang_data) );
+4. Untuk menampilkan struktur tabel yang telah dibuat kita gunakan perintah desc nama_tabel
+5. Untuk mengubah nama kolom gunakan perintah ALTER TABEL nama_tabel CHANGE kolom lama kolom baru type_data(value);
+6. Memasukkan data ke tabel kita gunakan perintah insert into nama_tabel value (“isi_kolom ”);
+7. Untuk menampilkan data yang telah dimasukkan gunakan perintah select * from nama_tabel;
+
